af1e48f4f5d88be79c42a4b6c351140ffd673237
Hitesh Sonwane <hiteshsonwane32@gmail.com>
2026-08-02 20:48:15 +0530

    login button

--- src/app/components/common/Navbar.jsx
+++ src/app/components/common/Navbar.jsx
@@ -13,18 +13,18 @@ export default function Navbar() {
   // redux cart
   const cartItems = useSelector((state) => state.cart.items)
   const cartItemCount = cartItems.reduce(
-    (total, item) => total + item.quantity, 
+    (total, item) => total + item.quantity,
     0
   );
 
-//  redux wishlist
-const wishlistItems = useSelector((state) => state.wishlist.items)
-const wishlistItemCount = wishlistItems.length;
+  //  redux wishlist
+  const wishlistItems = useSelector((state) => state.wishlist.items)
+  const wishlistItemCount = wishlistItems.length;
 
 
   return (
     <header>
-     <nav className="relative z-50 bg-white shadow-md px-6 py-4 flex items-center justify-between">
+      <nav className="relative z-50 bg-white shadow-md px-6 py-4 flex items-center justify-between">
         {/* logo */}
         <div className='flex flex-col leading-tight'>
           <Link href="/">
@@ -52,46 +52,54 @@ const wishlistItemCount = wishlistItems.length;
                 New Arrivals
               </Link>
             </li>
+
+            <li>
+              <Link href="/loginpage" className="">
+                Login
+              </Link>
+            </li>
           </ul>
+
+
         </div>
 
-          {/* wishlist, cart, profile */}
-          <div className='flex item-center gap-6 text-gray-700 text-xl'>
-            <div className='flex gap-6'>
-              <Link href="/wishlist">
-                <FaHeart className='hover:text-gray-500' />
-                {wishlistItemCount > 0 && (
-                  <span className='absolute -top-3 -right-4 text-xs text-white bg-[#a91f64] rounded-full px-1.5 py-0.5'>{wishlistItemCount}</span>
-                )}
-              </Link>
-
-              <Link href="/cart" className='relative '>
-                <FaShoppingCart className='hover:text-gray-500' />
-                <span className='absolute -top-3 -right-4 text-xs text-white bg-[#a91f64] rounded-full px-1.5 py-0.5'>{cartItemCount}</span>
-              </Link>
-
-              <Link href="/profile">
-                <FaUser className='hover:text-gray-400' />
-              </Link>
-            </div>
-          </div>
-
-          {/* menu icon */}
-          <div className='md:hidden flex '>
-            <button onClick={toggleMenu}>
-              {isMenuOpen ? (
-                <FaTimes className="text-2xl hover:text-[#a91f64]" />
-              ) : (
-                <FaBars className="text-2xl hover:text-[#a91f64]" />
+        {/* wishlist, cart, profile */}
+        <div className='flex item-center gap-6 text-gray-700 text-xl'>
+          <div className='flex gap-6'>
+            <Link href="/wishlist">
+              <FaHeart className='hover:text-gray-500' />
+              {wishlistItemCount > 0 && (
+                <span className='absolute -top-3 -right-4 text-xs text-white bg-[#a91f64] rounded-full px-1.5 py-0.5'>{wishlistItemCount}</span>
               )}
-            </button>
+            </Link>
+
+            <Link href="/cart" className='relative '>
+              <FaShoppingCart className='hover:text-gray-500' />
+              <span className='absolute -top-3 -right-4 text-xs text-white bg-[#a91f64] rounded-full px-1.5 py-0.5'>{cartItemCount}</span>
+            </Link>
+
+            <Link href="/profile">
+              <FaUser className='hover:text-gray-400' />
+            </Link>
           </div>
-      
+        </div>
+
+        {/* menu icon */}
+        <div className='md:hidden flex '>
+          <button onClick={toggleMenu}>
+            {isMenuOpen ? (
+              <FaTimes className="text-2xl hover:text-[#a91f64]" />
+            ) : (
+              <FaBars className="text-2xl hover:text-[#a91f64]" />
+            )}
+          </button>
+        </div>
+
         {/* mobile menu */}
         {isMenuOpen && <ul className='absolute top-full left-0 w-full bg-white flex flex-col item-center gap-4 py-4 text-gray-700 font-medium md:hidden shadow-md'>
           <li>
             <Link href="/" className="text-[#be4444da] " onClick={toggleMenu}>
-               Home
+              Home
             </Link>
           </li>
           <li>
@@ -109,7 +117,10 @@ const wishlistItemCount = wishlistItems.length;
               Top Seller
             </Link>
           </li>
-        </ul>}
+        </ul>
+
+        }
+
       </nav>
     </header>
   )
